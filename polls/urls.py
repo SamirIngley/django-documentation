0f1a9cf6f3c3ad13aade79cd1f50bee8ee81189e
SamirIngley <samir.ingle7@gmail.com>
2020-02-03 15:19:18 -0800

    class based views

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'polls'
 urlpatterns = [
+    path('now/', views.ShowTimeView.as_view(), name='now'),
+
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
 ]
+
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
+from datetime import datetime
+from .models import Question
+from django.views import View
 
 def index(request):
-    return HttpResponse("Hello Dani! Here's my polls index page.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    # context maps template variable names to python objects
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "you're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+class ShowTimeView(View):
+
+    def get(self, request):
+        now = datetime.now()
+        html = "<html><body>It is now {}<body></html>".format(now)
+        return HttpResponse(html)
+
